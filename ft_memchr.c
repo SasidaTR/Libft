5d1c5b64211d5172cd1a709f33dcb9d566d9659e
Tommy Rischmann <trischma@paul-f4Ar6s5.clusters.42paris.fr>
2024-05-16 13:29:21 +0200

    Push automatique :)

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,30 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-// Inclusion des bibliothèques nécessaires
-# include <stddef.h>
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*ptr;
+	unsigned char		uc;
 
-// Déclarations des fonctions
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-
-#endif
+	ptr = s;
+	uc = (unsigned char)c;
+	while (n > 0)
+	{
+		if (*ptr == uc)
+			return ((void *)ptr);
+		ptr++;
+		n--;
+	}
+	return (NULL);
+}
