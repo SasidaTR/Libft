aadf86c4dafc1c7a646d99c5b06d21c226bee65d
Tommy Rischmann <trischma@paul-f4Ar6s5.clusters.42paris.fr>
2024-05-16 12:41:55 +0200

    Push automatique :)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -10,28 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-// Inclusion des bibliothèques nécessaires
-# include <stddef.h>
+char	*ft_strrchr(const char *s, int c)
+{
+	const char	*last;
 
-// Déclarations des fonctions
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
-#endif
+	last = NULL;
+	while (*s)
+	{
+		if (*s == (char)c)
+			last = s;
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return ((char *)last);
+}
