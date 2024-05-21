9970e1ebb25177aec043354314d87fa626480825
Tommy Rischmann <trischma@paul-f4Ar6s5.clusters.42paris.fr>
2024-05-21 12:36:46 +0200

    Push automatique :)

--- ft_striteri.c
+++ ft_striteri.c
@@ -12,20 +12,16 @@
 
 #include "libft.h"
 
-static void	apply_function(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	while (s[i])
 	{
 		f(i, &s[i]);
 		i++;
 	}
-}
-
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
-{
-	if (s && f)
-		apply_function(s, f);
 }
