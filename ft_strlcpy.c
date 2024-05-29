68b714c7a3ae1f0d3e3c4408e3cb0aa9419a6b4e
Tommy Rischmann <trischma@paul-f4Ar4s4.clusters.42paris.fr>
2024-05-29 13:26:20 +0200

    Push automatique :)

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,14 +17,14 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
-	while (i + 1 < size && src[i] != '\0')
+	while (i + 1 < size && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	if (size > 0)
 		dest[i] = '\0';
-	while (src[i] != '\0')
+	while (src[i])
 		i++;
 	return (i);
 }
